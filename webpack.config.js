1d955a5461b1bfdfd3a4a787c5825f4da72065de
cy <xiongcaihu@qq.com>
2018-12-03 23:56:57 +0800

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
