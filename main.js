4512e7eb19ce1d67548f8004ac66bf5c8e50f8ba
cy <xiongcaihu@qq.com>
2019-10-08 18:37:48 +0800

    change config

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter,HashRouter} from "react-router-dom";
-import { renderRoutes } from "react-router-config";
-import routerConfig from './src/routerConfig.js';
 
-ReactDom.render(<BrowserRouter>
-  {renderRoutes(routerConfig)}
-</BrowserRouter>,
+ReactDom.render(<div>hello react</div>,
   document.querySelector('.app'));
--- src/home1/home1.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function(){
-    return <div>home1</div>
-}
